8227da72a0dd1e70c0d2d956c5e751cd5be46085
Valentyn Kovalskyi <kovalsky.valentine@gmail.com>
2025-04-13 15:52:35 +0300

    Add overflow behaviour

--- src/vTitle.js
+++ src/vTitle.js
@@ -3,12 +3,18 @@ const args = {
     ALWAYS: 'always',
     OVERFLOW: 'overflow',
 }
+/** 
+ * vTitle directive to set the title attribute of an element.
+ * @param {HTMLElement} el - The element to set the title attribute on.
+ */
 
 export const vTitle =  (el, binding, vnode) => {
     const content = el.textContent;
     if (binding.arg === args.ALWAYS || !binding.arg) {
         el.setAttribute('title', content);
     } else if (binding.arg === args.OVERFLOW) {
-        //TODO: Title appears only when the text is overflowing
+        if(el.scrollWidth > el.clientWidth) {
+            el.setAttribute('title', content);
+        }
     }
 }
